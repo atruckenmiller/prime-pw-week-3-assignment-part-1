88a4b3ae99f30e10d8e70dc281bce288f92f27b8
Andrew <adtruckenmiller@gmail.com>
2021-03-10 12:14:40 -0600

    Completed Assignment

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -38,8 +38,7 @@ console.log('Last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
-
+console.log('Number of animals:', animalArray[animalArray.length -1]);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
